daac00ae8c8c8c58fd2347abdfae04513da69432
Ahmad <ahmad.new.m.v@gmail.com>
2024-10-31 12:55:54 +0000

    Removed a bug

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -5,7 +5,7 @@ DELIMITER $$
 CREATE TRIGGER less_qty AFTER INSERT ON orders
 FOR EACH ROW 
 	BEGIN
-		IF items.name = NEW.item_nam THEN
+		IF items.name = NEW.item_nameee THEN
 			SET items.quantity = items.quantity - NEW.number;
 		END IF;
 	END$$

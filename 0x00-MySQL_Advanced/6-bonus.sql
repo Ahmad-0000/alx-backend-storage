313bc94c25a6c61f2bd8005b4c7aaa3e0463968f
Ahmad <ahmad.new.m.v@gmail.com>
2024-10-31 14:49:23 +0000

    7th task's solution

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,17 @@
+-- Creating a procedure
+
+DELIMITER $$
+
+CREATE PROCEDURE AddBouns(user_id INT, project_name VARCHAR(255), score INT)
+BEGIN
+	DECLARE project_id INT DEFAULT 0;
+	IF (SELECT COUNT(*) FROM projects WHERE name = project_name) = 0 THEN
+		INSERT INTO projects VALUES (NULL, project_name);
+		SET project_id = LAST_INSERT_ID();
+		INSERT INTO corrections VALUES (user_id, project_id, score);
+	ELSE
+		INSERT INTO corrections VALUES (user_id, (SELECT id FROM projects WHERE name = project_name), score);
+	END IF;
+END$$
+
+DELIMITER ;

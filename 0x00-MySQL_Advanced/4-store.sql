23d6c1c0edca54859071999e546240df77c7b264
Ahmad <ahmad.new.m.v@gmail.com>
2024-10-31 13:06:37 +0000

    New

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,13 +1,4 @@
 -- Decreasing quantity after each order
 
-DELIMITER $$
-
 CREATE TRIGGER less_qty BEFORE INSERT ON orders
-FOR EACH ROW 
-	BEGIN
-		IF items.name = NEW.item_name THEN
-			SET items.quantity = items.quantity - NEW.number;
-		END IF;
-	END$$
-
-DELIMITER ;
+FOR EACH ROW SET items.quantity = items.quantity - NEW.number;

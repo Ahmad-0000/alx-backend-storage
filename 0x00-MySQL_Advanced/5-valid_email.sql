0140c48eb40aa566443a5d80ab9d14861295bbe7
Ahmad <ahmad.new.m.v@gmail.com>
2024-10-31 14:13:56 +0000

    6th task's solution

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -4,7 +4,7 @@ DELIMITER $$
 
 CREATE TRIGGER c_email BEFORE UPDATE ON users FOR EACH ROW
 BEGIN
-	IF NEW.email REGEX '^[a-zA-Z\d_]+[a-zA-Z\d\._]*[a-zA-Z\d_]+@[a-zA-Z\d_]+[a-zA-Z\d_\-]*[a-zA-Z\d_]+\.[a-zA-Z\d_]+[a-zA-Z\d_\-]*[a-zA-Z\d_]+' THEN
+	IF NEW.email REGEXP '^[a-zA-Z\d_]+[a-zA-Z\d\._]*[a-zA-Z\d_]+@[a-zA-Z\d_]+[a-zA-Z\d_\-]*[a-zA-Z\d_]+\.[a-zA-Z\d_]+[a-zA-Z\d_\-]*[a-zA-Z\d_]+' THEN
 		SET NEW.valid_email = 1;
 	ELSE
 		SET NEW.valid_email = 0;

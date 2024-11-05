90e456e9a6e4335b9647b363525af986383dfa34
Ahmad <ahmad.new.m.v@gmail.com>
2024-11-05 06:46:24 +0000

    Last mandatory task's solution

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+13th task's solution
+"""
+import pymongo
+
+if __name__ == "__main__":
+    client = pymongo.MongoClient()
+    db = client.logs
+    col = db.nginx
+
+    print(f'{col.count_documents({})} logs\nMethods')
+    print(f'\tmethod GET: {col.count_documents({"method": "GET"})}')
+    print(f'\tmethod POST: {col.count_documents({"method": "POST"})}')
+    print(f'\tmethod PUT: {col.count_documents({"method": "PUT"})}')
+    print(f'\tmethod PATCH: {col.count_documents({"method": "PATCH"})}')
+    print(f'\tmethod DELETE: {col.count_documents({"method": "DELETE"})}')
+    status_check = col.count_documents({"method": "GET", "path": "/status"})
+    print(f'{status_check} status check')
+    client.close()

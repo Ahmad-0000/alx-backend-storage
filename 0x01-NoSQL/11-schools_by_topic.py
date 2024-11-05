b2527e15519e8e00def8eae76e6b71da55d67910
Ahmad <ahmad.new.m.v@gmail.com>
2024-11-05 06:05:26 +0000

    12th task's solution

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+12th task's solution
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """Returns a list of school names having a specific topic"""
+    doclist = []
+    for doc in collection.find():
+        if topic in doc.topics:
+            doclist.append(doc)
+    return doclist

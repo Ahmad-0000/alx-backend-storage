205382c27a18cfdf58e87be2430eed52a55f12b1
Ahmad <ahmad.new.m.v@gmail.com>
2024-11-05 06:11:02 +0000

    Removed a bug

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -8,6 +8,6 @@ def schools_by_topic(mongo_collection, topic):
     """Returns a list of school names having a specific topic"""
     doclist = []
     for doc in mongo_collection.find():
-        if topic in doc.topics:
+        if topic in doc["topics"]:
             doclist.append(doc)
     return doclist

7d922ec4583f6fb3d9266d39e6dca260fb5e1308
Ahmad <ahmad.new.m.v@gmail.com>
2024-11-06 11:54:03 +0000

    Removed redenducy

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -15,7 +15,7 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self: 'Cache', data: Union[str, bytes, float, int]) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """Storing data into redis server"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)

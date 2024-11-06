a66cd7184584540d0faf351d348f1cd9b2311cb5
Ahmad <ahmad.new.m.v@gmail.com>
2024-11-06 14:53:54 +0000

    Optional task's solution

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Web scrapper
+"""
+import redis
+import requests
+
+
+def get_page(url: str) -> str:
+    """Getting a resource located in url"""
+    r = redis.Redis()
+    key = f"count:{url}"
+    r.incr(key, 1)
+    r.expire(key, 10)
+    fun = requests.request
+    result = fun('GET', url)
+    return result.text

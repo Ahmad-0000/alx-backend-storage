192c3bdbd2c337d7d26089dcbd8c791acd596e24
Ahmad <ahmad.new.m.v@gmail.com>
2024-11-06 14:59:09 +0000

    Added a correct key

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -9,7 +9,7 @@ import requests
 def get_page(url: str) -> str:
     """Getting a resource located in url"""
     r = redis.Redis()
-    key = f"count:{url}"
+    key = f"count:{{{url}}}"
     fun = requests.request
     result = fun('GET', url)
     r.incr(key, 1)
